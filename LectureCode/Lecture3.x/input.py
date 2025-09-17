bb9cc13fe8a161ecafda7ed69d5c5b113d535d70
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-09-17 08:14:17 +0300

    Covered Input and ouput

---
+++ LectureCode/Lecture3.x/input.py
@@ -0,0 +1,7 @@
+#Input(): This function is used to take input from the user.
+#The function returns a string value.
+name=input("Enter your name:")
+print(f"Hello {name}")
+
+email=input("Enter your email:")
+print(f"Your email is {email}")
---
+++ LectureCode/Lecture3.x/output.py
@@ -0,0 +1,18 @@
+name="Joel" 
+age=200
+print("My name is ",name,"Age is ",age)
+print(f"My name is {name} Age is {age}")
+
+print("Apple", "Banana", "Mango", sep=", ", end=".\n")
+#sep is used to separate the values
+#end is used to end the line with a specific character
+#Apple, Banana, Mango.
+
+print("Hello world")
+print("Hello", end=".\n")
+print("Uganda","Kenya","Tanzania", sep=",", end=".\n")
+
+name="James"
+score=87.9
+print(f"Name: {name}, Score: {score}%")
+print("Name:{} and score is {}%".format(name,score))
